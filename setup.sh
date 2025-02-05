76572ea3ef68504cc60ea5a72818b5b6dfcec5e3
jtama <j.tama@groupeonepoint.com>
2025-02-05 20:10:29 +0100

    light highlight

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ cd slides
 git clone https://github.com/hakimel/reveal.js --depth 1 --branch 5.1.0
 git clone https://github.com/denehyg/reveal.js-menu --depth 1 --branch 2.1.0 reveal.js/plugin/menu
 cd reveal.js && npm i && npm run build && cd ../
-mkdir highlight.js
 curl -sLo highlight.js/highlight.min.js https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.3/highlight.min.js
+curl -sLo highlight.js/atom-one-light.min.css https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.3/styles/atom-one-light.min.css
